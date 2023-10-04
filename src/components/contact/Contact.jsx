778ce78436efc367aad9971054513830bd35fe42
Adeyemo Ademola <adeyemoademola12@gmail.com>
2023-10-04 23:03:54 +0100

    First Commit

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsLinkedin } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Us</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>adeyemoademola12@gmail.com</h5>
+            <a href="mailto:adeyemoademola12@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsLinkedin className="contact__option-icon" />
+            <h4>LinkedIn</h4>
+            <h5>Ademola Adeyemo</h5>
+            <a
+              href="https://www.linkedin.com/in/ademola-adeyemo"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+234 913 5451 448</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=2349135451448"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Hi-Clean - The Best Cleaning Services in Nigeria",
+    github: "https://github.com",
+    demo: "https://www.hicleanservices.com.ng/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Coloby - Create, Collaborate and Innovate",
+    github: "https://github.com",
+    demo: "https://colobyapp.netlify.app/",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Bit Digital University get digital freedom",
+    github: "https://github.com",
+    demo: "https://bitdigitaluniversity.com/",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Hi-Clean - The Best Cleaning Services in Nigerian",
+    github: "https://github.com",
+    demo: "https://www.hicleanservices.com.ng/",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Coloby - Create, Collaborate and Innovate",
+    github: "https://github.com",
+    demo: "https://colobyapp.netlify.app/",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Bit Digital University get digital freedom",
+    github: "https://github.com",
+    demo: "https://bitdigitaluniversity.com/",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
